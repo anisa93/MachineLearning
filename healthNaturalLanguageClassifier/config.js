55b7952d0faa2892cfd6de4732dbb61073af20c1
anisa93 <nisa.adiba@gmail.com>
2017-12-04 22:20:38 -0500

    commiting final copy to github

---
+++ healthNaturalLanguageClassifier/config.js
@@ -0,0 +1,11 @@
+var config = {
+	watson: {
+natural_language_classifier: {
+  username: "d7fce1fa-f9ad-49ea-a433-beac97179823",
+  password: "ulyDS12Nw1oF",
+  version: "v1",
+  id: "e554c3x251-nlc-29612"
+}
+	}
+};
+module.exports = config;
